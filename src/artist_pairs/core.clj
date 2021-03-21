4afc617946e8c782731564d7c0306b20166f6c53
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2021-03-21 16:57:21 -0700

    Update core ns to output CSV based on input

--- src/artist_pairs/core.clj
+++ src/artist_pairs/core.clj
@@ -1,4 +1,9 @@
-(ns artist-pairs.core)
+(ns artist-pairs.core
+  (:require [artist-pairs.counter :refer [count-pairs]]
+            [artist-pairs.csv-writer :refer [write-pairs]]))
 
-(defn -main [x]
-  (println x "Hello, World!"))
+(defn -main [input-path output-path]
+  (->> input-path
+       count-pairs
+       (filter (fn [[pair appearances]] (> appearances 49)))
+       (write-pairs output-path)))
